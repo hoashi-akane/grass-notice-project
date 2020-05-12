de602d805cd4d98776e74cf52856b85088e04682
HOASHI AKANE <51166726+hoashi-akane@users.noreply.github.com>
2020-05-13 05:22:14 +0900

    修正：現在時刻の外部依存を排除

--- grass.go
+++ grass.go
@@ -38,9 +38,8 @@ func handler(){
 }
 // 関数名(引数)(戻り値errも渡せる)
 func utcToJst(hub []githubEvent)(c int) {
-	loc, _ := time.LoadLocation("Asia/Tokyo")
+	time.Local = time.FixedZone("Local", 9*60*60)
 	now := time.Now()
-	now = now.Truncate( time.Hour ).Add( - time.Duration(now.Hour()) * time.Hour )
 	// kは要素番号 vは内容 (key,value)
 	for _, v := range hub {
 		// JSTで作成（まだ表示される時間はUTC)
